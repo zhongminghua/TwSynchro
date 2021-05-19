244350345403708c17e3eceb3f387642d93c0acf
pengdesign <393668663@qq.com>
2021-05-19 14:20:38 +0800

    同步

--- TwSynchro/Utils/TimestampHelp.cs
+++ TwSynchro/Utils/TimestampHelp.cs
@@ -1,8 +1,7 @@
 ﻿using Dapper;
 using System;
-using System.Collections.Concurrent;
 using System.Data;
-using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
 using Utils;
 
@@ -11,11 +10,6 @@ namespace TwSynchro.Utils
     public static class TimestampHelp
     {
         /// <summary>
-        /// 时间戳缓存
-        /// </summary>
-        private static readonly ConcurrentDictionary<string, string> _timestampList = new();
-
-        /// <summary>
         /// 读取时间戳
         /// </summary>
         /// <param name="sqlServerConn"></param>
@@ -23,26 +17,22 @@ namespace TwSynchro.Utils
         /// <returns></returns>
         public static async Task<string> GetTimestampAsync(IDbConnection sqlServerConn, string key)
         {
-            string timestamp=string.Empty;
+            object timestamp = CacheHelper.CacheValue(key);
 
-            if (CacheHelper.CacheValue(key)!=null)
+            if (timestamp is null)
             {
-                timestamp = Convert.ToDateTime(CacheHelper.CacheValue(key)).ToString("yyyy-MM-dd HH:mm:ss");
-            }
-            else
-            {
-                string sql= $@"SELECT TOP 1 TimestampKey, TimestampValue FROM Tb_Sys_SynchroTimestamp_MySql WHERE TimestampKey='{key}'";
 
-                var timestampdata = await sqlServerConn.QueryAsync<(string TimestampKey, DateTime TimestampValue)>(sql);
+                string sql = $@"SELECT TOP 1 TimestampValue FROM Tb_Sys_SynchroTimestamp_MySql WHERE TimestampKey='{key}'";
 
-                if (timestampdata.AsList().Count>0)
-                    timestamp = timestampdata.AsList()[0].TimestampValue.ToString("yyyy-MM-dd HH:mm:ss");
+                var data = (await sqlServerConn.QueryAsync<string>(sql)).ToList();
+
+                if (data.Count > 0)
+                    timestamp = DateTime.Parse(data[0]).ToString("yyyy-MM-dd HH:mm:ss");
                 else
                     timestamp = DateTime.Now.AddYears(-10).ToString("yyyy-MM-dd HH:mm:ss");
 
             }
-
-            return timestamp;
+            return timestamp.ToString();
         }
 
         /// <summary>
@@ -52,31 +42,21 @@ namespace TwSynchro.Utils
         /// <param name="key"></param>
         /// <param name="value">时间戳值</param>
         /// <param name="minute">绝对过期时间（分钟）</param>
-        public static async void SetTimestamp(IDbConnection sqlServerConn, string key, string value, int minute)
+        public static async void SetTimestampAsync(IDbConnection sqlServerConn, string key, string value, int minute)
         {
 
-            // _timestampList.AddOrUpdate(key, _ => value, (_, _) => value);
+            CacheHelper.CacheInsertFromMinutes(key, value, minute);
 
-            CacheHelper.CacheInsertAddMinutes(key, value, minute);
+            string sql = $@"SELECT COUNT(1) FROM Tb_Sys_SynchroTimestamp_MySql WHERE TimestampKey='{key}'";
 
-            StringBuilder sql = new($@"SELECT COUNT(1) FROM Tb_Sys_SynchroTimestamp_MySql WHERE TimestampKey='{key}'");
+            int count = sqlServerConn.QuerySingle<int>(sql.ToString());
 
-            int i = sqlServerConn.QuerySingle<int>(sql.ToString());
-
-            sql.Clear();
-
-            if (i > 1)
-            {
-                sql.Append($@"UPDATE Tb_Sys_SynchroTimestamp_MySql SET TimestampValue='{value}' WHERE TimestampKey='{key}'");
-
-                await sqlServerConn.ExecuteAsync(sql.ToString());
-            }
+            if (count > 1)
+                sql = $@"UPDATE Tb_Sys_SynchroTimestamp_MySql SET TimestampValue='{value}' WHERE TimestampKey='{key}'";
             else
-            {
-                sql.Append($@"INSERT INTO Tb_Sys_SynchroTimestamp_MySql(TimestampKey,TimestampValue) VALUES ('{key}','{value}')");
+                sql = $@"INSERT INTO Tb_Sys_SynchroTimestamp_MySql(TimestampKey,TimestampValue) VALUES ('{key}','{value}')";
 
-                await sqlServerConn.ExecuteAsync(sql.ToString());
-            }
+            await sqlServerConn.ExecuteAsync(sql.ToString());
 
         }
 
