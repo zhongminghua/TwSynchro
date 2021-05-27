20d03297be8521e00c8b438d9ea9f60bd569ce3a
pengdesign <393668663@qq.com>
2021-05-27 18:49:27 +0800

    同步

--- TwSynchro/Worker.cs
+++ TwSynchro/Worker.cs
@@ -74,9 +74,9 @@ namespace TwSynchro
         {
             try
             {
-                //await Task.WhenAll(new[] { RunTaskOrganize(stoppingToken) });
+                //await Task.WhenAll(new[] { RunTaskCustomer(stoppingToken) });
 
-                await Task.WhenAll(new[] { RunTaskUser(stoppingToken), RunTaskOrganize(stoppingToken), RunTaskCustomer(stoppingToken), RunTaskCustomer(stoppingToken),
+                await Task.WhenAll(new[] { RunTaskUser(stoppingToken), RunTaskOrganize(stoppingToken), RunTaskCustomer(stoppingToken),
                 RunTaskMenu(stoppingToken), RunTaskOrganizeUser(stoppingToken), RunTaskMenuUser(stoppingToken), RunTaskPermission(stoppingToken),
                 RunTaskCostItem(stoppingToken), RunTaskTaxRateSetting(stoppingToken), RunTaskResource(stoppingToken)});
             }
